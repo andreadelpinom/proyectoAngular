e0c002bb5b7197329beecfb9aa6e63b28eaab3d4
andreadelpinom <andreadelpino@uees.edu.ec>
2025-02-10 18:12:27 -0500

    Actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { VentanasComponent } from './shared/ventanas/ventanas.component';
 import { RecursosService } from './servicios/recursos.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './shared/footer/footer.component';
+import { Foto } from './interfaz/foto';
 
 
 @Component({
@@ -20,5 +21,10 @@ import { FooterComponent } from './shared/footer/footer.component';
 
 export class AppComponent {
   title = 'proyectoAngular';
-  constructor(private recursosService: RecursosService){}
+  fotos: Foto[] = [];
+  constructor(private recursosService: RecursosService){
+    recursosService.obtenerDatos().subscribe(respuesta => {
+      this.fotos = respuesta as Array<Foto>
+    } )
+  }
 }
